c0b09a5aa0778b62f5355afe52865de6d1c796a4
taka1202 <opoylvm.122@gmail.com>
2023-09-10 00:20:00 +0900

    完成

--- 3rd_session/js/Main.js
+++ 3rd_session/js/Main.js
@@ -1,6 +1,14 @@
 window.onload = function() {
-function copyText() {
-    text = document.getElementsByClassName("text");
-}
+    const bt = document.getElementsByClassName('button');
+    
+    bt[0].addEventListener('click', () => {
+        const textarea = document.getElementById('textarea');
+        const answer = document.getElementById('answer');
+        answer.innerText = textarea.value;
+    },false)
 
 }
+// function copyText() {
+//     text = document.getElementsByClassName("button");
+//     console.log("てすと");
+// }
